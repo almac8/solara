696a32dcc0c627e8f34a2649a1500254fddbe49e
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-09-20 23:47:26 +0200

    Material extraction and returning with the drone

--- Assets/Scripts/Units/Drone.cs
+++ Assets/Scripts/Units/Drone.cs
@@ -6,14 +6,24 @@ public class Drone : Unit {
   [SerializeField] private float rotationSpeed;
   [SerializeField] private float movementSpeed;
 
+  [SerializeField] private float cargoStorageCapacity = 5f;
+  [SerializeField] private float cargoStorage = 0f;
+  [SerializeField] private float extractionRate = 0.5f;
+
   private enum DroneState {
     DOCKED,
     MOVING,
-    COLLECTING
+    COLLECTING,
+    HOMING
   }
 
-  private DroneState state;
+  [SerializeField] private DroneState state;
   private GameObject target;
+  private Vector3 homingPoint;
+
+  private void Start() {
+    homingPoint = transform.position;
+  }
 
   private void Update() {
     switch(state) {
@@ -30,11 +40,22 @@ public class Drone : Unit {
           Vector3 direction = Vector3.Normalize(collectionPoint - transform.position);
           transform.Translate(direction * Time.deltaTime * movementSpeed);
         }
-        
         break;
 
       case DroneState.COLLECTING:
         transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+        cargoStorage = Mathf.Clamp(cargoStorage + extractionRate * Time.deltaTime, 0, cargoStorageCapacity);
+        if(cargoStorage == cargoStorageCapacity) state = DroneState.HOMING;
+        break;
+
+      case DroneState.HOMING:
+        if(transform.position == homingPoint) {
+          state = DroneState.DOCKED;
+        } else {
+          transform.eulerAngles = Vector3.zero;
+          Vector3 direction = Vector3.Normalize(homingPoint - transform.position);
+          transform.Translate(direction * Time.deltaTime * movementSpeed);
+        }
         break;
     }
   }
