20148aef4f1d0c99a94ff33b8e139f1ea9550777
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-13 16:22:24 +0200

    Preparations

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ModuleMatrix))]
 public class Unit : Selectable {
   [SerializeField] private HUD hud;
 
