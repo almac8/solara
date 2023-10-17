37d8b942f8f01fa915cfdf1192e53ce561d0d0f9
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-17 16:29:08 +0200

    Construction Module no longer needs construction manager to be set manually

--- Assets/Scripts/ModuleMatrix/Modules/Construction.cs
+++ Assets/Scripts/ModuleMatrix/Modules/Construction.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Construction : Module {
-  [SerializeField] private ConstructionManager constructionManager;
-
   private void Awake() {
     Title = "Construction Module";
     Description = "It can build stuff";
     Activator = new ModuleActivator(false, "Stop Construction", "Start Construction");
+  }
+
+  private void Start() {
+    ConstructionManager constructionManager = GameObject.FindWithTag("ConstructionManager").GetComponent<ConstructionManager>();
     Activator.Activated += constructionManager.EnableConstructionMode;
     Activator.Deactivated += constructionManager.DisableConstructionMode;
   }
