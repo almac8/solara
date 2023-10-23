1e0a0a29922e1a16e4687aa2747a49cd77d938f5
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 19:17:01 +0200

    TopographyScanner Module Requirements

--- Assets/Scripts/ModuleMatrix/Modules/TopographyScanner.cs
+++ Assets/Scripts/ModuleMatrix/Modules/TopographyScanner.cs
@@ -3,35 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TopographyScanner : Module {
-  public float ScanCompletion { get; private set; }
+  private const int POWER_STORAGE_INDEX = 0;
+  private const int DATA_STORAGE_INDEX = 1;
+  
+  [SerializeField] private float scanRate;
+  [SerializeField] private float powerRequired;
+  [SerializeField] private float completeScanDataSize;
 
-  public float powerRequired;
-  public float scanRate;
-  public float completeScanDataSize;
-  public PowerStorage powerStorage;
-  public DataStorage dataStorage;
+  public float ScanCompletion { get; private set; }
 
   private void Start() {
     Title = "Topography Scanner";
     Description = "\"MapMaster 5000 - Uncovering the Universe's Dirty Secrets.\" Bringing you the topographical dirt on every celestial body, one scan at a time.";
     Activator = new ModuleActivator(false, "Disable Topography Scanner", "Enable Topography Scanner");
-    powerStorage = gameObject.GetComponent<PowerStorage>();
-    dataStorage = gameObject.GetComponent<DataStorage>();
+
+    ModuleRequirement powerStorageRequirement = new ModuleRequirement();
+    powerStorageRequirement.SetRequiredModule<PowerStorage>("Power Storage");
+
+    ModuleRequirement dataStorageRequirement = new ModuleRequirement();
+    dataStorageRequirement.SetRequiredModule<DataStorage>("Data Storaage");
+
+    Requirements.Add(powerStorageRequirement);
+    Requirements.Add(dataStorageRequirement);
   }
   
   public override void RunStep(float deltaTime) {
     if(Activator.IsActive) {
-      float samplePowerRequirement = powerRequired * deltaTime;
+      PowerStorage powerStorage = Requirements[POWER_STORAGE_INDEX].AssociatedModule as PowerStorage;
+      DataStorage dataStorage = Requirements[DATA_STORAGE_INDEX].AssociatedModule as DataStorage;
 
-      if(powerStorage.DrainCharge(samplePowerRequirement)) {
-        float scanSampleCompletion = scanRate * Time.deltaTime;
-        float sampleDataRequirement = scanSampleCompletion * completeScanDataSize;
+      if(powerStorage != null && dataStorage != null) {
+        float samplePowerRequirement = powerRequired * deltaTime;
 
-        if(dataStorage.WriteData(sampleDataRequirement)) {
-          ScanCompletion = Mathf.Clamp(ScanCompletion + scanSampleCompletion, 0f, 1f);
-          if(ScanCompletion == 1f) Activator.Toggle();
-        } else {
-          powerStorage.SupplyCharge(samplePowerRequirement);
+        if(powerStorage.DrainCharge(samplePowerRequirement)) {
+          float scanSampleCompletion = scanRate * Time.deltaTime;
+          float sampleDataRequirement = scanSampleCompletion * completeScanDataSize;
+
+          if(dataStorage.WriteData(sampleDataRequirement)) {
+            ScanCompletion = Mathf.Clamp(ScanCompletion + scanSampleCompletion, 0f, 1f);
+            if(ScanCompletion == 1f) Activator.Toggle();
+          } else {
+            powerStorage.SupplyCharge(samplePowerRequirement);
+          }
         }
       }
     }
