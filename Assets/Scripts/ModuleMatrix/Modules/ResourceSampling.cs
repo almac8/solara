439fcd34c7c41908490ed6560418a97615bf4d69
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 18:07:06 +0200

    Resource Sampling Module Requirements

--- Assets/Scripts/ModuleMatrix/Modules/ResourceSampling.cs
+++ Assets/Scripts/ModuleMatrix/Modules/ResourceSampling.cs
@@ -3,27 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ResourceSampling : Module {
+  private const int TARGETING_INDEX = 0;
+  
   [SerializeField] private float sampleSize;
   [SerializeField] private float samplingRate;
-
-  private Targeting targeting;
 
   private void Awake() {
     Title = "Resource Sampling";
     Description = "Collects samples of resources";
     Activator = new ModuleActivator(false, "Cancel Sample", "Collect Sample");
     Gauge = new ModuleGauge(0f, sampleSize, "Sample");
-    targeting = GetComponent<Targeting>();
+
+    ModuleRequirement requirement = new ModuleRequirement();
+    requirement.SetRequiredModule<Targeting>("Targeting");
   }
 
   public override void RunStep(float deltaTime) {
-    if(Activator.IsActive && targeting.IsLockedOn) {
-      Gauge.Value += samplingRate * deltaTime;
+    if(Activator.IsActive) {
+      Targeting targeting = Requirements[TARGETING_INDEX].AssociatedModule as Targeting;
 
-      if(Gauge.Value > sampleSize) {
-        Gauge.Value = sampleSize;
-        Activator.Toggle();
-        //  Store the sample in physical sorage
+      if(targeting != null && targeting.IsLockedOn) {
+        Gauge.Value += samplingRate * deltaTime;
+
+        if(Gauge.Value > sampleSize) {
+          Gauge.Value = sampleSize;
+          Activator.Toggle();
+          //  Store the sample in physical sorage
+        }
       }
     }
   }
