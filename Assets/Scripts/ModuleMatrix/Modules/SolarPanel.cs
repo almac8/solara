ebdcd9ee02c1af938daff241048b358d1451bd82
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 18:14:03 +0200

    Solar Panel Module Requirements

--- Assets/Scripts/ModuleMatrix/Modules/SolarPanel.cs
+++ Assets/Scripts/ModuleMatrix/Modules/SolarPanel.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SolarPanel : Module {
-  public float rechargeRate;
-  public float rechargeEfficiency;
-  public PowerStorage powerStorage;
-  public Animator animator;
+  private const int POWER_STORAGE_INDEX = 0;
+  
+  [SerializeField] private float rechargeRate;
+  [SerializeField] private float rechargeEfficiency;
+
+  private Animator animator;
 
   private void Start() {
     Title = "Solar Panel";
     Description = "\"Solar Slurper - Sipping Sunshine Since Forever.\" Absorbing sunlight to keep our systems running, because why not harness the power of a giant, flaming ball of gas?";
-    powerStorage = gameObject.GetComponent<PowerStorage>();
     Activator = new ModuleActivator(false, "Deactivate Solar Panel", "Activate Solar Panel");
+
+    ModuleRequirement requirement = new ModuleRequirement();
+    requirement.SetRequiredModule<PowerStorage>("Power Storage");
   }
 
   public override void RunStep(float deltaTime) {
-    float chargeSupplied = Activator.IsActive ? rechargeRate * rechargeEfficiency * deltaTime : 0f;
-    powerStorage.SupplyCharge(chargeSupplied);
+    if(Activator.IsActive) {
+      PowerStorage powerStorage = Requirements[POWER_STORAGE_INDEX].AssociatedModule as PowerStorage;
+      float chargeSupplied = 0f;
 
-    animator.SetBool("is_deployed", Activator.IsActive);
+      if(powerStorage != null) {
+        chargeSupplied = rechargeRate * rechargeEfficiency * deltaTime;
+        powerStorage.SupplyCharge(chargeSupplied);
+      }
+      
+      animator.SetBool("is_deployed", Activator.IsActive);
+    }
   }
 }
