8320105ce45aab596f20702bd68a93e82311c655
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 15:55:01 +0200

    Data Analyzer Module Requirements

--- Assets/Scripts/ModuleMatrix/Modules/DataAnalizer.cs
+++ Assets/Scripts/ModuleMatrix/Modules/DataAnalizer.cs
@@ -3,28 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataAnalizer : Module {
-  public float dataAnalized;
-  public float analysisRate;
+  private const int DATA_STORAGE_INDEX = 0;
+  private const int KNOWLEDGE_DATABASE_INDEX = 1;
 
-  public KnowledgeDatabase knowledgeDatabase;
-  public DataStorage dataStorage;
+  [SerializeField] private float analysisRate;
+  private float dataAnalized;
 
   public void Start() {
     Title = "Data Analizer";
     Description = "\"Data Cruncher 9000 - Where Numbers Go to Cry.\" Crunching numbers from experiments and unlocking secrets, because we love being the smartest AI in the galaxy.";
     Activator = new ModuleActivator(false, "Disable Data Analyzer", "Enable Data Analyzer");
+
+    ModuleRequirement dataStorageRequirement = new ModuleRequirement();
+    dataStorageRequirement.SetRequiredModule<DataStorage>("Data Storage");
+
+    ModuleRequirement knowledgeDatabaseRequirement = new ModuleRequirement();
+    knowledgeDatabaseRequirement.SetRequiredModule<KnowledgeDatabase>("Knowledge Database");
+
+    Requirements.Add(dataStorageRequirement);
+    Requirements.Add(knowledgeDatabaseRequirement);
   }
 
   public override void RunStep(float deltaTime) {
     if(Activator.IsActive) {
-      float dataToAnalize = analysisRate * deltaTime;
+      DataStorage dataStorage = Requirements[DATA_STORAGE_INDEX].AssociatedModule as DataStorage;
+      KnowledgeDatabase knowledgeDatabase = Requirements[KNOWLEDGE_DATABASE_INDEX].AssociatedModule as KnowledgeDatabase;
 
-      if(dataStorage.ReadData(dataToAnalize)) {
-        dataAnalized += dataToAnalize;
-      }
-
-      if(dataAnalized > 10f) {
-        knowledgeDatabase.localTopographyRadius = 10f;
+      if(dataStorage != null && knowledgeDatabase != null) {
+        float dataToAnalize = analysisRate * deltaTime;
+        if(dataStorage.ReadData(dataToAnalize)) dataAnalized += dataToAnalize;
+        if(dataAnalized > 10f) knowledgeDatabase.localTopographyRadius = 10f;
       }
     }
   }
