095fcc0be7533bda6bf9d3d64193848c9a8020ba
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 17:37:44 +0200

    Made DataStorage storageCapacity private

--- Assets/Scripts/ModuleMatrix/Modules/DataStorage.cs
+++ Assets/Scripts/ModuleMatrix/Modules/DataStorage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataStorage : Module {
-  public float storageCapacity;
+  [SerializeField] private float storageCapacity;
 
   private float dataWritten;
   private float dataProcessed;
