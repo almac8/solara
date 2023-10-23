7df1c44448168b1ef90d90131ef9faa41b8c79ea
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 17:39:58 +0200

    DroneDock Removed topography scanner

--- Assets/Scripts/ModuleMatrix/Modules/DroneDock.cs
+++ Assets/Scripts/ModuleMatrix/Modules/DroneDock.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class DroneDock : Module {
   public GameObject drone;
-  public TopographyScanner topographyScanner;
   public Animator animator;
-
+  
   private void Start() {
     Title = "Drone Dock";
     Description = "\"Drone Deployment Depot - Your Personal Taskmasters.\" Because we know Solara can't be bothered to pick up space rocks herself.";
-    topographyScanner = gameObject.GetComponent<TopographyScanner>();
     Activator = new ModuleActivator(false, "Dock Drone", "Deploy Drone");
 
     Activator.Deactivated += () => {
