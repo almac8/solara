5e15038f2d6d869a0220e0402a87cb60c20b316e
Aimée MacDonald <aimeelmacdonald@pm.me>
2023-10-23 17:46:39 +0200

    Homing Module Requirements

--- Assets/Scripts/ModuleMatrix/Modules/Homing.cs
+++ Assets/Scripts/ModuleMatrix/Modules/Homing.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Homing : Module {
-  private Targeting targeting = null;
-  private HoverMovement hoverMovement = null;
   private GameObject homeDock = null;
 
   private void Awake() {
     Title = "Homing Module";
     Description = "Takes us Home";
     Activator = new ModuleActivator(false, "Cancel Homing", "Go Home");
-    targeting = GetComponent<Targeting>();
+    
+    ModuleRequirement requirement = new ModuleRequirement();
+    requirement.SetRequiredModule<Targeting>("Targeting");
+    Requirements.Add(requirement);
+
     homeDock = transform.parent.gameObject;
-    hoverMovement = GetComponent<HoverMovement>();
   }
 
   public override void RunStep(float deltaTime) {
     if(Activator.IsActive){
-      targeting.SetTarget(homeDock);
+      Requirements[TARGETING_INDEX].SetTarget(homeDock);
     }
   }
 }
