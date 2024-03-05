31905a7383ceb0eee7e7431027fe6cf0d40e2bde
Aimée MacDonald <aimeelmacdonald@pm.me>
2024-03-05 11:30:00 +0200

    Blueprint floors

---
+++ Assets/Scripts/Blueprint.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blueprint : MonoBehaviour {
+  [SerializeField] private GameObject bpBlankReference;
+  [SerializeField] private GameObject bpFloorReference;
+  [SerializeField] private GameObject bpFloorCornerReference;
+  
+  [SerializeField] private int numTilesWidth;
+  [SerializeField] private int numTilesHeight;
+
+  private GameObject[,] tiles;
+
+  private enum TileType {
+    BLANK,
+    FLOOR,
+    FLOOR_CORNER
+  }
+
+  private TileType currentlySelectedTile;
+  private Quaternion currentlySelectedTileRotation;
+
+  private void Start() {
+    tiles = new GameObject[numTilesWidth, numTilesHeight];
+    currentlySelectedTile = TileType.BLANK;
+    currentlySelectedTileRotation = bpBlankReference.transform.rotation;
+    
+    for(int x = 0; x < numTilesWidth; x++) {
+      for(int y = 0; y < numTilesHeight; y++) {
+        InitializeTile(x, y);
+      }
+    }
+  }
+
+  private void Update() {
+    if(Input.GetKeyDown(KeyCode.O)) {
+      int currentlySelectedInt = (int) currentlySelectedTile;
+      currentlySelectedInt--;
+
+      if(currentlySelectedInt < 0) {
+        currentlySelectedInt = 2;
+      }
+
+      currentlySelectedTile = (TileType) currentlySelectedInt;
+    }
+
+    if(Input.GetKeyDown(KeyCode.P)) {
+      int currentlySelectedInt = (int) currentlySelectedTile;
+      currentlySelectedInt++;
+
+      if(currentlySelectedInt > 2) {
+        currentlySelectedInt = 0;
+      }
+
+      currentlySelectedTile = (TileType) currentlySelectedInt;
+    }
+
+    if(Input.GetKeyDown(KeyCode.R)) {
+      Quaternion rotation = Quaternion.Euler(0f, 0f, 90f);
+      currentlySelectedTileRotation = currentlySelectedTileRotation * rotation;
+    }
+  }
+
+  public void TileClicked(int x, int y) {
+    SetTile(x, y, currentlySelectedTile);
+  }
+
+  private void InitializeTile(int x, int y) {
+    Vector3 offset = new Vector3(x - numTilesWidth / 2, y - numTilesHeight / 2, 0);
+    tiles[x, y] = GameObject.Instantiate(bpBlankReference, offset, currentlySelectedTileRotation, transform);
+    SetTile(x, y, TileType.BLANK);
+  }
+
+  private void SetTile(int x, int y, TileType tile) {
+    GameObject newTile;
+
+    switch (tile) {
+      case TileType.BLANK:
+        newTile = GameObject.Instantiate(bpBlankReference, tiles[x, y].transform.position, currentlySelectedTileRotation, transform);
+        break;
+
+      case TileType.FLOOR:
+        newTile = GameObject.Instantiate(bpFloorReference, tiles[x, y].transform.position, currentlySelectedTileRotation, transform);
+        break;
+      
+      case TileType.FLOOR_CORNER:
+        newTile = GameObject.Instantiate(bpFloorCornerReference, tiles[x, y].transform.position, currentlySelectedTileRotation, transform);
+        break;
+
+      default:
+        newTile = GameObject.Instantiate(bpBlankReference, tiles[x, y].transform.position, currentlySelectedTileRotation, transform);
+        break;
+    }
+
+    BlueprintTile tileScript = newTile.GetComponent<BlueprintTile>();
+    tileScript.SetIndex(x, y);
+    Destroy(tiles[x, y]);
+    tiles[x, y] = newTile;
+  }
+}
